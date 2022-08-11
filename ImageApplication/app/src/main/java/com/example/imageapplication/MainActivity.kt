f97ec0d191e997e9b0cd4c7cc2499ee7ce4f0a7a
Tanima <tanimasultana.du@gmail.com>
2022-08-11 14:26:27 +0600

    onactivity result issue replaced with registerForActivityResult

--- ImageApplication/app/src/main/java/com/example/imageapplication/MainActivity.kt
+++ ImageApplication/app/src/main/java/com/example/imageapplication/MainActivity.kt
@@ -1,16 +1,11 @@
 package com.example.imageapplication
 
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Point
-import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.os.Environment
-import android.provider.Settings
 import android.util.Log
 import android.view.Window
 import android.widget.Button
@@ -32,6 +27,7 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var cropImageLuncher: ActivityResultLauncher<Intent>
     private lateinit var floatImage: ActivityResultLauncher<String>
     private lateinit var simpleFloatingWindow: SimpleFloatingWindow
     private var uri: Uri? = null
@@ -40,21 +36,11 @@ class MainActivity : AppCompatActivity() {
     private var btnGallery: Button? = null
 
     private var sourceImage: ImageView? = null
-    var selectedImage: Uri? = null
-    private val RESULT_LOAD_IMAGE = 1
-    var chunkSideLength = 50
-
-    var chunkedImage: java.util.ArrayList<Bitmap>? = null
-
-    // Number of rows and columns in chunked image
-    var rows = 0
-    var cols = 0 // Number of rows and columns in chunked image
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
-        //alertDialogForCameraImage();
         btnGallery = findViewById(R.id.btn_image)
         sourceImage = findViewById(R.id.iv_souceImage)
         split_image = findViewById(R.id.btn_splitImage)
@@ -68,6 +54,18 @@ class MainActivity : AppCompatActivity() {
 //
 //        }
 
+
+        cropImageLuncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if(it.resultCode == Activity.RESULT_OK ){
+                val result = it.data?.getStringExtra("RESULT")
+                var resultUri:Uri? = null
+                if (result != null){
+                    resultUri = Uri.parse(result)
+                }
+
+                sourceImage?.setImageURI(resultUri)
+            }
+        }
 
         val loadImage = registerForActivityResult(ActivityResultContracts.GetContent()){
             if(it != null){
@@ -93,7 +91,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun createImageFile():File{
+    private fun createImageFile():File{
         val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
         val imageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile("Specimen_${timestamp}",".jpg",imageDirectory).apply {
@@ -110,7 +108,7 @@ class MainActivity : AppCompatActivity() {
         android.Manifest.permission.READ_EXTERNAL_STORAGE
     )
 
-    private fun takepicture(){
+    private fun takePicture(){
 
         createImageFile().also {
             try {
@@ -144,20 +142,9 @@ class MainActivity : AppCompatActivity() {
     private fun cropImage(uri: Uri?) {
         val intent = Intent(this,MergedImage::class.java)
         intent.putExtra("DATA",uri?.toString())
-        startActivityForResult(intent,101)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if( resultCode == -1 && requestCode == 101){
-            val result = data?.getStringExtra("RESULT")
-            var resultUri:Uri? = null
-            if (result != null){
-                resultUri = Uri.parse(result)
-            }
-
-            sourceImage?.setImageURI(resultUri)
-        }
+        setResult(101,intent)
+        cropImageLuncher.launch(intent)
+        //startActivityForResult(intent,101)
     }
 
     private fun pickImageFromGallery(loadImage: ActivityResultLauncher<String>) {
@@ -182,7 +169,7 @@ class MainActivity : AppCompatActivity() {
                 android.Manifest.permission.WRITE_EXTERNAL_STORAGE),1)
             }else{
 
-                takepicture()
+                takePicture()
             }
         }
 
@@ -195,332 +182,4 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
-    private fun splitImage(image: ImageView, chunkNumbers: Int) {
-
-        //For the number of rows and columns of the grid to be displayed
-        val rows: Int
-        val cols: Int
-
-        //For height and width of the small image chunks
-        val chunkHeight: Int
-        val chunkWidth: Int
-
-        //To store all the small image chunks in bitmap format in this list
-        val chunkedImages = ArrayList<Bitmap>(chunkNumbers)
-
-        //Getting the scaled bitmap of the source image
-        val drawable = image.drawable as BitmapDrawable
-        val bitmap = drawable.bitmap
-        val scaledBitmap = Bitmap.createScaledBitmap(bitmap, bitmap.width, bitmap.height, true)
-        cols = Math.sqrt(chunkNumbers.toDouble()).toInt()
-        rows = cols
-        chunkHeight = bitmap.height / rows
-        chunkWidth = bitmap.width / cols
-
-        //xCoord and yCoord are the pixel positions of the image chunks
-        var yCoord = 0
-        for (x in 0 until rows) {
-            var xCoord = 0
-            for (y in 0 until cols) {
-                chunkedImages.add(
-                    Bitmap.createBitmap(
-                        scaledBitmap,
-                        xCoord,
-                        yCoord,
-                        chunkWidth,
-                        chunkHeight
-                    )
-                )
-                xCoord += chunkWidth
-            }
-            yCoord += chunkHeight
-        }
-
-        sourceImage?.setImageBitmap(chunkedImage?.get(0))
-
-        /* Now the chunkedImages has all the small image chunks in the form of Bitmap class.
-         * You can do what ever you want with this chunkedImages as per your requirement.
-         * I pass it to a new Activity to show all small chunks in a grid for demo.
-         * You can get the source code of this activity from my Google Drive Account.
-         */
-        //Start a new activity to show these chunks into a grid
-//        val intent = Intent(this@ImageActivity, ChunkedImageActivity::class.java)
-//        intent.putParcelableArrayListExtra("image chunks", chunkedImages)
-//        startActivity(intent)
-    }
-
-    private fun splitImage1(image: ImageView, chunkSideLength: Int) {
-        val random = Random(System.currentTimeMillis())
-
-        // height and weight of higher|wider chunks if they would be
-        val higherChunkSide: Int
-        val widerChunkSide: Int
-
-        // Getting the scaled bitmap of the source image
-        val bitmap = (image.drawable as BitmapDrawable).bitmap
-        rows = bitmap.height / chunkSideLength
-        higherChunkSide = bitmap.height % chunkSideLength + chunkSideLength
-        cols = bitmap.width / chunkSideLength
-        widerChunkSide = bitmap.width % chunkSideLength + chunkSideLength
-
-        // To store all the small image chunks in bitmap format in this list
-        chunkedImage = ArrayList(rows * cols)
-        if (higherChunkSide != chunkSideLength) {
-            if (widerChunkSide != chunkSideLength) {
-                // picture has both higher and wider chunks plus one big square chunk
-                val widerChunks = ArrayList<Bitmap?>(rows - 1)
-                val higherChunks = ArrayList<Bitmap?>(cols - 1)
-                val squareChunk: Bitmap
-                var yCoord = 0
-                for (y in 0 until rows - 1) {
-                    var xCoord = 0
-                    for (x in 0 until cols - 1) {
-                        chunkedImage!!.add(
-                            Bitmap.createBitmap(
-                                bitmap,
-                                xCoord,
-                                yCoord,
-                                chunkSideLength,
-                                chunkSideLength
-                            )
-                        )
-                        xCoord += chunkSideLength
-                    }
-                    // add last chunk in a row to array of wider chunks
-                    widerChunks.add(
-                        Bitmap.createBitmap(
-                            bitmap,
-                            xCoord,
-                            yCoord,
-                            widerChunkSide,
-                            chunkSideLength
-                        )
-                    )
-                    yCoord += chunkSideLength
-                }
-
-                // add last row to array of higher chunks
-                var xCoord = 0
-                for (x in 0 until cols - 1) {
-                    higherChunks.add(
-                        Bitmap.createBitmap(
-                            bitmap,
-                            xCoord,
-                            yCoord,
-                            chunkSideLength,
-                            higherChunkSide
-                        )
-                    )
-                    xCoord += chunkSideLength
-                }
-
-                //save bottom-right big square chunk
-                squareChunk =
-                    Bitmap.createBitmap(bitmap, xCoord, yCoord, widerChunkSide, higherChunkSide)
-
-                //shuffle arrays
-                Collections.shuffle(chunkedImage)
-                Collections.shuffle(higherChunks)
-                Collections.shuffle(widerChunks)
-
-                //determine random position of big square chunk
-                val bigChunkX = random.nextInt(cols)
-                val bigChunkY = random.nextInt(rows)
-
-                //add wider and higher chunks into resulting array of chunks
-                //all wider(higher) chunks should be in one column(row) to avoid collisions between chunks
-                //We must insert it row by row because they will displace each other from their columns otherwise
-                for (y in 0 until rows - 1) {
-                    chunkedImage!!.add(cols * y + bigChunkX, widerChunks[y]!!)
-                }
-
-                //And then we insert the whole row of higher chunks
-                for (x in 0 until cols - 1) {
-                    chunkedImage!!.add(bigChunkY * cols + x, higherChunks[x]!!)
-                }
-                chunkedImage!!.add(bigChunkY * cols + bigChunkX, squareChunk)
-            } else {
-                // picture has only number of higher chunks
-                val higherChunks = ArrayList<Bitmap?>(cols)
-                var yCoord = 0
-                for (y in 0 until rows - 1) {
-                    var xCoord = 0
-                    for (x in 0 until cols) {
-                        chunkedImage!!.add(
-                            Bitmap.createBitmap(
-                                bitmap,
-                                xCoord,
-                                yCoord,
-                                chunkSideLength,
-                                chunkSideLength
-                            )
-                        )
-                        xCoord += chunkSideLength
-                    }
-                    yCoord += chunkSideLength
-                }
-
-                // add last row to array of higher chunks
-                var xCoord = 0
-                for (x in 0 until cols) {
-                    higherChunks.add(
-                        Bitmap.createBitmap(
-                            bitmap,
-                            xCoord,
-                            yCoord,
-                            chunkSideLength,
-                            higherChunkSide
-                        )
-                    )
-                    xCoord += chunkSideLength
-                }
-
-                //shuffle arrays
-                Collections.shuffle(chunkedImage)
-                Collections.shuffle(higherChunks)
-
-                //add higher chunks into resulting array of chunks
-                //Each higher chunk should be in his own column to preserve original image size
-                //We must insert it row by row because they will displace each other from their columns otherwise
-                val higherChunksPositions: MutableList<Point> = ArrayList<Point>(cols)
-                for (x in 0 until cols) {
-                    higherChunksPositions.add(Point(x, random.nextInt(rows)))
-                }
-
-                //sort positions of higher chunks. THe upper-left elements should be first
-
-                Collections.sort(higherChunksPositions, object : Comparator<Point> {
-                    override fun compare(lhs: Point, rhs: Point): Int {
-                        if (lhs.y != rhs.y) {
-                            return if (lhs.y < rhs.y) -1 else 1
-                        } else if (lhs.x != rhs.x) {
-                            return if (lhs.x < rhs.x) -1 else 1
-                        }
-                        return 0
-                    }
-                })
-//                Collections.sort(higherChunksPositions, Comparator<Any?> { lhs, rhs ->
-//                    if (lhs.y !== rhs.y) {
-//                        return@Comparator if (lhs.y < rhs.y) -1 else 1
-//                    } else if (lhs.x !== rhs.x) {
-//                        return@Comparator if (lhs.x < rhs.x) -1 else 1
-//                    }
-//                    0
-//                })
-                for (x in 0 until cols) {
-                    val currentCoord: Point = higherChunksPositions[x]
-                    chunkedImage!!.add(currentCoord.y * cols + currentCoord.x, higherChunks[x]!!)
-                }
-            }
-        } else {
-            if (widerChunkSide != chunkSideLength) {
-                // picture has only number of wider chunks
-                val widerChunks = ArrayList<Bitmap?>(rows)
-                var yCoord = 0
-                for (y in 0 until rows) {
-                    var xCoord = 0
-                    for (x in 0 until cols - 1) {
-                        chunkedImage!!.add(
-                            Bitmap.createBitmap(
-                                bitmap,
-                                xCoord,
-                                yCoord,
-                                chunkSideLength,
-                                chunkSideLength
-                            )
-                        )
-                        xCoord += chunkSideLength
-                    }
-                    // add last chunk in a row to array of wider chunks
-                    widerChunks.add(
-                        Bitmap.createBitmap(
-                            bitmap,
-                            xCoord,
-                            yCoord,
-                            widerChunkSide,
-                            chunkSideLength
-                        )
-                    )
-                    yCoord += chunkSideLength
-                }
-
-                //shuffle arrays
-                Collections.shuffle(chunkedImage)
-                Collections.shuffle(widerChunks)
-
-                //add wider chunks into resulting array of chunks
-                //Each wider chunk should be in his own row to preserve original image size
-                for (y in 0 until rows) {
-                    chunkedImage!!.add(cols * y + random.nextInt(cols), widerChunks[y]!!)
-                }
-            } else {
-                // picture perfectly splits into square chunks
-                var yCoord = 0
-                for (y in 0 until rows) {
-                    var xCoord = 0
-                    for (x in 0 until cols) {
-                        chunkedImage!!.add(
-                            Bitmap.createBitmap(
-                                bitmap,
-                                xCoord,
-                                yCoord,
-                                chunkSideLength,
-                                chunkSideLength
-                            )
-                        )
-                        xCoord += chunkSideLength
-                    }
-                    yCoord += chunkSideLength
-                }
-                Collections.shuffle(chunkedImage)
-            }
-        }
-
-        // Function of merge the chunks images(after image divided in pieces then i can call this function to combine
-        // and merge the image as one)
-        mergeImage(chunkedImage!!, bitmap.width, bitmap.height)
-    }
-
-    private fun mergeImage(imageChunks: ArrayList<Bitmap>, width: Int, height: Int) {
-
-        // create a bitmap of a size which can hold the complete image after merging
-        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_4444)
-
-        // create a canvas for drawing all those small images
-        val canvas = Canvas(bitmap)
-        var count = 0
-        var currentChunk = imageChunks[0]
-
-        //Array of previous row chunks bottom y coordinates
-        val yCoordinates = IntArray(cols)
-        Arrays.fill(yCoordinates, 0)
-        for (y in 0 until rows) {
-            var xCoord = 0
-            for (x in 0 until cols) {
-                currentChunk = imageChunks[count]
-                canvas.drawBitmap(currentChunk,xCoord.toFloat(),yCoordinates[x].toFloat(),null)
-                //canvas.drawBitmap(currentChunk, xCoord.toFloat(), yCoordinates[x], null)
-                xCoord += currentChunk.width
-                yCoordinates[x] += currentChunk.height
-                count++
-            }
-        }
-
-        /*
-     * The result image is shown in a new Activity
-     */
-//        val intent = Intent(this@MainActivity, MergedImage::class.java)
-//        intent.putExtra("merged_image", bitmap)
-//        startActivity(intent)
-//        finish()
-
-        Log.d("MergedImage","merged image in bitmap :$bitmap")
-        sourceImage?.setImageBitmap(chunkedImage?.get(1))
-    }
-
-
-    companion object {
-        private const val REQUEST_CODE_DRAW_OVERLAY_PERMISSION = 5
-    }
 }
--- ImageApplication/app/src/main/java/com/example/imageapplication/MergedImage.kt
+++ ImageApplication/app/src/main/java/com/example/imageapplication/MergedImage.kt
@@ -1,19 +1,15 @@
 package com.example.imageapplication
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.snackbar.Snackbar
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.imageapplication.databinding.ActivityMergedImageBinding
 import com.yalantis.ucrop.UCrop
 import java.io.File
-import java.lang.StringBuilder
 import java.util.*
 
 class MergedImage : AppCompatActivity() {
@@ -34,14 +30,29 @@ class MergedImage : AppCompatActivity() {
 
         val options:UCrop.Options = UCrop.Options()
 
-        fileUri?.let {
+        val a = fileUri?.let {
             UCrop.of(it,Uri.fromFile(File(cacheDir,destinationUri)))
                 .withOptions(options)
                 .withAspectRatio(0F, 0F)
                 .useSourceImageAspectRatio()
                 .withMaxResultSize(2000,2000)
-                .start(this)
+                .getIntent(this)
+                //.start(this)
         }
+
+        val b = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result1 ->
+            if( result1.resultCode == Activity.RESULT_OK ){
+                val resultUri = result1.data?.let { UCrop.getOutput(it) }
+                val resultIntent = Intent()
+                resultIntent.putExtra("RESULT","$resultUri")
+                setResult(-1,resultIntent)
+                finish()
+            }else if(result1.resultCode == UCrop.RESULT_ERROR){
+                val cropError = result1.data?.let { UCrop.getError(it) }
+            }
+        }
+
+        b.launch(a)
 
     }
 
@@ -54,16 +65,4 @@ class MergedImage : AppCompatActivity() {
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP){
-            val resultUri = data?.let { UCrop.getOutput(it) }
-            val resultIntent = Intent()
-            resultIntent.putExtra("RESULT","$resultUri")
-            setResult(-1,resultIntent)
-            finish()
-        }else if(resultCode == UCrop.RESULT_ERROR){
-            val cropError = data?.let { UCrop.getError(it) }
-        }
-    }
 }
