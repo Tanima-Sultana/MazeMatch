ce4fc9313e22d09c1891a16dfbfc2014a8034618
Tanima <tanimasultana.du@gmail.com>
2022-08-17 17:47:03 +0600

    firebase storage and realtime database introduced

--- ImageApplication/app/src/main/java/com/example/imageapplication/ImageUploadFragment.kt
+++ ImageApplication/app/src/main/java/com/example/imageapplication/ImageUploadFragment.kt
@@ -12,6 +12,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.MimeTypeMap
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
@@ -22,6 +23,11 @@ import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import com.example.imageapplication.databinding.FragmentImageUploadBinding
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import java.io.File
 import java.io.InputStream
 import java.text.SimpleDateFormat
@@ -40,6 +46,9 @@ class ImageUploadFragment : Fragment() {
 
     private var sourceImage: ImageView? = null
 
+    private lateinit var mStorageRef:StorageReference
+    private lateinit var mDatabaseRef:DatabaseReference
+
 
     companion object {
         var PERMISSIONS = arrayOf(
@@ -47,12 +56,13 @@ class ImageUploadFragment : Fragment() {
             Manifest.permission.WRITE_EXTERNAL_STORAGE
         )
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        binding = FragmentImageUploadBinding.inflate(inflater,container,false)
+        binding = FragmentImageUploadBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -62,28 +72,31 @@ class ImageUploadFragment : Fragment() {
 
         btnGallery = binding.btnImage
         sourceImage = binding.ivSouceImage
+        mStorageRef = FirebaseStorage.getInstance().getReference("uploads")
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("uploads")
 
-        cropImageLuncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            if(it.resultCode == Activity.RESULT_OK ){
-                val result = it.data?.getStringExtra("RESULT")
-                var resultUri:Uri? = null
-                if (result != null){
-                    resultUri = Uri.parse(result)
+        cropImageLuncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == Activity.RESULT_OK) {
+                    val result = it.data?.getStringExtra("RESULT")
+                    var resultUri: Uri? = null
+                    if (result != null) {
+                        resultUri = Uri.parse(result)
+                    }
+
+                    sourceImage?.setImageURI(resultUri)
                 }
-
-                sourceImage?.setImageURI(resultUri)
             }
-        }
 
-        loadImage = registerForActivityResult(ActivityResultContracts.GetContent()){
-            if(it != null){
-                Log.d("MainActivity","image data : $it and ${it.path}")
+        loadImage = registerForActivityResult(ActivityResultContracts.GetContent()) {
+            if (it != null) {
+                Log.d("MainActivity", "image data : $it and ${it.path}")
 
-                if (getImageSize(it)!! <200){
+                if (getImageSize(it)!! < 200) {
                     sourceImage?.setImageURI(it)
                     cropImage(it)
-                }else{
-                    showMessageOKCancel("Your image is above 200kb",null)
+                } else {
+                    showMessageOKCancel("Your image is above 200kb", null)
                 }
             }
         }
@@ -95,43 +108,45 @@ class ImageUploadFragment : Fragment() {
 
 
     private fun cropImage(uri: Uri?) {
-        val intent = Intent(requireContext(),MergedImage::class.java)
-        intent.putExtra("DATA",uri?.toString())
-        requireActivity().setResult(101,intent)
+        val intent = Intent(requireContext(), MergedImage::class.java)
+        intent.putExtra("DATA", uri?.toString())
+        requireActivity().setResult(101, intent)
         //setResult(101,intent)
         cropImageLuncher.launch(intent)
     }
 
-    private val takePicture = registerForActivityResult(ActivityResultContracts.TakePicture()) { success: Boolean ->
-        if (success) {
-            Log.d("CameraImage","Image location :$uri")
-            if (getImageSize(uri!!)!! <200){
-                sourceImage?.setImageURI(uri)
-                cropImage(uri)
-            }else{
-                Toast.makeText(requireContext(),"Your image is above 200kb",
-                Toast.LENGTH_SHORT).show()
-                //showToast("Your image is above 200kb")
+    private val takePicture =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) { success: Boolean ->
+            if (success) {
+                Log.d("CameraImage", "Image location :$uri")
+                if (getImageSize(uri!!)!! < 200) {
+                    sourceImage?.setImageURI(uri)
+                    cropImage(uri)
+                } else {
+                    Toast.makeText(
+                        requireContext(), "Your image is above 200kb",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    //showToast("Your image is above 200kb")
+                }
+                // The image was saved into the given Uri -> do something with it
+
+            } else {
+                Log.d("CameraImage", "Image is not saved")
+
             }
-            // The image was saved into the given Uri -> do something with it
-
-        }else{
-            Log.d("CameraImage","Image is not saved")
-
         }
-    }
 
 
-
-    private fun takePicture(){
+    private fun takePicture() {
 
         createImageFile().also {
             try {
                 uri = FileProvider.getUriForFile(
-                    requireContext(),BuildConfig.APPLICATION_ID+".provider",it)
-            }
-            catch (e:Exception){
-                Log.e("CameraImage","Error: ${e.message}")
+                    requireContext(), BuildConfig.APPLICATION_ID + ".provider", it
+                )
+            } catch (e: Exception) {
+                Log.e("CameraImage", "Error: ${e.message}")
             }
         }
 
@@ -141,11 +156,12 @@ class ImageUploadFragment : Fragment() {
     }
 
     val requestMultiplePermissions = registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()) { permission ->
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permission ->
         val granted = permission.entries.all {
             it.value
         }
-        if(granted) {
+        if (granted) {
             loadImage.launch("image/*")
 
             //pickImageFromGallery()
@@ -155,56 +171,46 @@ class ImageUploadFragment : Fragment() {
     val requestSinglePermission = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
     ) {
-        if (it){
+        if (it) {
             takePicture()
         }
     }
 
 
+    private fun pickImageFromGallery() {
 
-        private fun pickImageFromGallery() {
 
+        val alertDialog = AlertDialog.Builder(requireContext())
+        alertDialog.setTitle("Select ")
+        alertDialog.setPositiveButton("Pick Image") { _, _ ->
+            if (hasExternalReadPermission() != PackageManager.PERMISSION_GRANTED) {
+                requestSinglePermission.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
 
-            val alertDialog = AlertDialog.Builder(requireContext())
-            alertDialog.setTitle("Select ")
-            alertDialog.setPositiveButton("Pick Image"){ _, _ ->
-                if( hasExternalReadPermission() != PackageManager.PERMISSION_GRANTED){
-//                    ActivityCompat.requestPermissions(requireActivity(), arrayOf(
-//                        Manifest.permission.CAMERA,
-//                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
-//                        Manifest.permission.READ_EXTERNAL_STORAGE),2)
-                    requestSinglePermission.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
-
-                }else{
-                    loadImage.launch("image/*")
-
-                }
-            }
-
-            alertDialog.setNegativeButton("Camera"){ _, _ ->
-                if(hasCameraPermission() != PackageManager.PERMISSION_GRANTED ||
-                    hasExternalWritePermission() != PackageManager.PERMISSION_GRANTED ){
-                    requestMultiplePermissions.launch(PERMISSIONS)
-
-//                    ActivityCompat.requestPermissions(requireActivity(), arrayOf(
-//                        Manifest.permission.CAMERA,
-//                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
-//                        Manifest.permission.READ_EXTERNAL_STORAGE),1)
-                }else{
-
-                    takePicture()
-                }
-            }
-
-            alertDialog.setNeutralButton("Cancel"){ _, _ ->
+            } else {
+                loadImage.launch("image/*")
 
             }
+        }
 
-            val builder = alertDialog.create()
-            builder.show()
+        alertDialog.setNegativeButton("Camera") { _, _ ->
+            if (hasCameraPermission() != PackageManager.PERMISSION_GRANTED ||
+                hasExternalWritePermission() != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestMultiplePermissions.launch(PERMISSIONS)
+            } else {
+
+                takePicture()
+            }
+        }
+
+        alertDialog.setNeutralButton("Cancel") { _, _ ->
 
         }
 
+        val builder = alertDialog.create()
+        builder.show()
+
+    }
 
 
     private fun showMessageOKCancel(message: String, okListener: DialogInterface.OnClickListener?) {
@@ -216,70 +222,33 @@ class ImageUploadFragment : Fragment() {
             .show()
     }
 
-    private fun hasCameraPermission() = ContextCompat.checkSelfPermission(requireContext(),
+    private fun hasCameraPermission() = ContextCompat.checkSelfPermission(
+        requireContext(),
         Manifest.permission.CAMERA
     )
-    private fun hasExternalWritePermission() = ContextCompat.checkSelfPermission(requireContext(),
-        Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
-    private fun hasExternalReadPermission() = ContextCompat.checkSelfPermission(requireContext(),
+    private fun hasExternalWritePermission() = ContextCompat.checkSelfPermission(
+        requireContext(),
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+    private fun hasExternalReadPermission() = ContextCompat.checkSelfPermission(
+        requireContext(),
         Manifest.permission.READ_EXTERNAL_STORAGE
     )
 
     private fun createImageFile(): File {
         val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
         val imageDirectory = requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile("Specimen_${timestamp}",".jpg",imageDirectory).apply {
+        return File.createTempFile("Specimen_${timestamp}", ".jpg", imageDirectory).apply {
             currentImagePath = absolutePath
         }
     }
 
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<out String>,
-//        grantResults: IntArray
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if(grantResults.isNotEmpty()){
-//            val cameraAccepted = grantResults[0]== PackageManager.PERMISSION_GRANTED
-//            val readStorageAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED
-//            val writeExternalAccepted = grantResults[2] == PackageManager.PERMISSION_GRANTED
-//
-//            if(cameraAccepted && writeExternalAccepted){
-//                Log.d("Permission","all permission are accepted")
-//                takePicture()
-//            }else if(readStorageAccepted){
-//                loadImage.launch("image/*")
-//            }else{
-//                Snackbar.make(requireView(),"Permission denied,",Snackbar.LENGTH_LONG).show()
-//                //showToast("Permission denied")
-//
-//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                    if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
-//                        showMessageOKCancel("You need to allow access to both the permissions"
-//                        ) { _, _ ->
-//                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                                requestPermissions(
-//                                    arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-//                                        android.Manifest.permission.READ_EXTERNAL_STORAGE,
-//                                        Manifest.permission.CAMERA
-//                                    ),
-//                                    1
-//                                )
-//                            }
-//                        }
-//                        return
-//                    }
-//                }
-//
-//            }
-//        }
-//
-//    }
-
-    private fun getImageSize(uri: Uri):Int? {
+    private fun getImageSize(uri: Uri): Int? {
         return try {
-            val aaa:InputStream? = this@ImageUploadFragment.context?.contentResolver?.openInputStream(uri)
+            val aaa: InputStream? =
+                this@ImageUploadFragment.context?.contentResolver?.openInputStream(uri)
             //val aaa: InputStream = requireContext().contentResolver.openInputStream(uri)
             val byteSize: Int? = aaa?.available()
             val kbSize = byteSize?.div(1024)
@@ -288,6 +257,31 @@ class ImageUploadFragment : Fragment() {
         } catch (e: java.lang.Exception) {
             // here you can handle exception here
             null
+        }
+    }
+
+    private fun getFileExtension(uri:Uri):String?{
+        val cr = requireActivity().contentResolver
+        val mime = MimeTypeMap.getSingleton()
+        return mime.getExtensionFromMimeType(cr.getType(uri))
+    }
+
+    private fun uploadFile(){
+        if(sourceImage?.isDirty == true && uri != null){
+
+            val fileReference = mStorageRef.child("uploads/"+System.currentTimeMillis()
+            +"."+ uri?.let { getFileExtension(it) })
+            fileReference.putFile(uri!!)
+                .addOnSuccessListener {
+
+
+                }.addOnFailureListener{
+
+                }.addOnProgressListener {
+                    val progress = (it.bytesTransferred / it.totalByteCount) * 100.0
+                }
+        }else{
+            Toast.makeText(requireContext(),"No file to upload",Toast.LENGTH_SHORT).show()
         }
     }
 
